702da8a2e91f60fa3880091bd6d7d9a263ca1b2a
sb <sb@sb.com>
2023-04-28 12:06:24 -0600

    get modules script

---
+++ scripts/getModuleNames.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+const nodeModulesPath = path.join(process.cwd(), 'node_modules');
+
+fs.readdir(nodeModulesPath, { withFileTypes: true }, (err, entries) => {
+  if (err) {
+    console.error('Error al leer el directorio node_modules:', err);
+    return;
+  }
+
+  const folderNames = entries
+    .filter(entry => entry.isDirectory())
+    .map(entry => entry.name);
+
+  console.log('Carpetas en node_modules:');
+  console.log(folderNames.join('\n'));
+});
